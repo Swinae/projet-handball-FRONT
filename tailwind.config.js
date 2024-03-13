4daec05c27109cc10949b09ae6a95f187cce6eef
jordan <jordannigthwatch@hotmail.com>
2024-03-13 14:50:16 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}','node_modules/flowbite-react/lib/esm/**/*.js','./node_modules/flowbite/**/*.js'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
