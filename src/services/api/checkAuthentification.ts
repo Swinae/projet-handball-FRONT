eafb099e40c20b79588ed43e42ef04fb8f6648d1
jordan <jordannigthwatch@hotmail.com>
2024-03-19 14:48:40 +0100

    update: création fichier checkAutehentification.ts

--- src/services/api/checkAuthentification.ts
+++ src/services/api/checkAuthentification.ts
@@ -3,6 +3,6 @@ import { useApi } from "../hooks/useApi";
 const api=useApi();
 
 export async function checkAuthentification(userLoginData:any){
-  const response = await api.post(``,userLoginData);
-  console.log("response :",response)
+  //const response = await api.post(``,userLoginData);
+  //console.log("response :",response)
 }
