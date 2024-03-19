ee9549b56c80a3da5c87c0187593f62023c3a4ab
Alexandre BALPE <alexandre.balpe@gmail.com>
2024-03-19 14:38:42 +0100

    Update : - CSS in App.css - Tailwind property in login modal

--- src/components/Navbar/NavBar.tsx
+++ src/components/Navbar/NavBar.tsx
@@ -1,21 +1,21 @@
 import { NavLink } from 'react-router-dom';
 import './NavBar.css';
 
-type PropsNavbar={
-  userRole?:string;
+type PropsNavbar = {
+  userRole?: string;
 }
 
-export function NavBar({userRole="visiteur"}:PropsNavbar) {
-    return (
-        <>
-            <nav className='bg-custom-287581 text-white nav-features'>
-              <NavLink to="/actualités" className={({isActive})=>(isActive?"active":undefined)}>Actualités</NavLink>
-              <NavLink to="/évènements" className={({isActive})=>(isActive?"active":undefined)}>Evènements</NavLink>    
-              <NavLink to="/résultats-des-matchs" className={({isActive})=>(isActive?"active":undefined)}>Résultats des matchs</NavLink>    
-              {userRole === "admin" && <NavLink to="/calendrier" className={({isActive})=>(isActive?"active":undefined)}>Calendrier</NavLink>}    
-              {userRole === "admin" && <NavLink to="/profil-des-joueurs" className={({isActive})=>(isActive?"active":undefined)}>Profil des joueurs</NavLink>}    
-              {userRole === "admin" && <NavLink to="/tableau-de-bord" className={({isActive})=>(isActive?"active":undefined)}>Tableau de bord</NavLink>}    
-            </nav>
-        </>
-    )
+export function NavBar({ userRole = "visiteur" }: PropsNavbar) {
+  return (
+    <>
+      <nav className='bg-custom-287581 text-white nav-features'>
+        <NavLink to="/actualités" className={({ isActive }) => (isActive ? "active" : undefined)}>Actualités</NavLink>
+        <NavLink to="/évènements" className={({ isActive }) => (isActive ? "active" : undefined)}>Evènements</NavLink>
+        <NavLink to="/résultats-des-matchs" className={({ isActive }) => (isActive ? "active" : undefined)}>Résultats des matchs</NavLink>
+        {userRole === "admin" && <NavLink to="/calendrier" className={({ isActive }) => (isActive ? "active" : undefined)}>Calendrier</NavLink>}
+        {userRole === "admin" && <NavLink to="/profil-des-joueurs" className={({ isActive }) => (isActive ? "active" : undefined)}>Profil des joueurs</NavLink>}
+        {userRole === "admin" && <NavLink to="/tableau-de-bord" className={({ isActive }) => (isActive ? "active" : undefined)}>Tableau de bord</NavLink>}
+      </nav>
+    </>
+  )
 }
