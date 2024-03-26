f1d989bd7dfa8312b11e3bc82b0a4e11f199ad04
Alexandre BALPE <alexandre.balpe@gmail.com>
2024-03-26 17:15:42 +0100

    ASSO_NW-88 Created : - Dashboard Page - Tabs for each section of the dashboard - EventTab componant layout

--- src/pages/Dashboard/DashboardPage.tsx
+++ src/pages/Dashboard/DashboardPage.tsx
@@ -1,18 +1,17 @@
 import { Tabs } from "flowbite-react";
+import EventsTab from "../../components/tabs/EventsTab/EventsTab";
 
 export default function DashboardPage() {
     return (
         <>
-            <Tabs aria-label="Tabs with icons" style="underline">
+            <Tabs aria-label="Tabs" style="underline">
                 <Tabs.Item active title="Gérer les actualités">
                     This is <span className="font-medium text-gray-800 dark:text-white">Profile tab's associated content</span>.
                     Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
                     control the content visibility and styling.
                 </Tabs.Item>
                 <Tabs.Item title="Gérer les évènements">
-                    This is <span className="font-medium text-gray-800 dark:text-white">Dashboard tab's associated content</span>.
-                    Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
-                    control the content visibility and styling.
+                    {<EventsTab />}
                 </Tabs.Item>
                 <Tabs.Item title="Gérer les utilisateurs">
                     This is <span className="font-medium text-gray-800 dark:text-white">Settings tab's associated content</span>.
