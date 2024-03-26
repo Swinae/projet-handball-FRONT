301d3a07413e4e8ce7de4d9ae0b0360ed1c1b3a5
jordan <jordannigthwatch@hotmail.com>
2024-03-26 14:13:12 +0100

    ASSO_NW-70 : implémenter le design et la logique pour des données dynamique et pour passer à la page suivante ou précédente

--- src/components/tabs/NewTab/NewTab.tsx
+++ src/components/tabs/NewTab/NewTab.tsx
@@ -2,27 +2,37 @@ import { Table, TableBody, TableCell, TableHead, TableHeadCell, TableRow } from 
 import { getNewsList } from "../../../services/api/getNewsList";
 import { useEffect, useState } from "react";
 import { NewData } from "../../../services/interfaces/NewData";
-
-/*à faire: mettre en place la logique pour afficher maximum 6 premieres actua
-puis en cliquant sur suivant il affiche les autres
-*/
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 export function NewTab() {
-  const[newsList,setNewsList]=useState<NewData[]>();
+  const [newsList, setNewsList] = useState<NewData[]>([]);
+  const [startIndex, setStartIndex] = useState<number>(0);
+  const newsNbrPerPage:number = 4;
 
-  useEffect(()=>{
-    const getNewsListRequest=async()=>{
-      const response= await getNewsList();
-      if(response){
+  useEffect(() => {
+    const getNewsListRequest = async () => {
+      const response = await getNewsList();
+      if (response) {
         //update NewsList
         setNewsList(response);
       }
-      else{
+      else {
         console.log("la requête n'a pas abouti");
       }
     }
     getNewsListRequest();
-  },[])  
+  }, [])
+
+  const visibleNewsList: NewData[] | undefined = newsList?.slice(startIndex, startIndex + newsNbrPerPage);
+
+  const handleNext = () => {
+    setStartIndex(startIndex + newsNbrPerPage);
+  }
+
+  const handlePrevious=()=>{
+    setStartIndex(Math.max(0, startIndex - newsNbrPerPage));
+  }
 
   return (
     <div className="overflow-x-auto mb-6">
@@ -31,38 +41,60 @@ export function NewTab() {
         Créer une nouvelle actualité
       </button>
 
-      <Table hoverable>
+      <Table hoverable className="min-w-96">
         <TableHead className="text-white">
           <TableHeadCell className='bg-custom-15616D' >Titre</TableHeadCell>
-          <TableHeadCell className='bg-custom-15616D' >Date de publication</TableHeadCell>
-          <TableHeadCell className='bg-custom-15616D' >Actions</TableHeadCell>
+          <TableHeadCell className='bg-custom-15616D w-1/6' >Date de publication</TableHeadCell>
+          <TableHeadCell className='bg-custom-15616D w-1/6' >Actions</TableHeadCell>
         </TableHead>
+
         <TableBody className="divide-y">
-          {newsList?.map((art) => {
+          {visibleNewsList?.map((art) => {
             return (
               <TableRow key={art.id} className="bg-white dark:border-gray-700 dark:bg-gray-800">
                 <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
                   {art.title}
                 </TableCell>
-                <TableCell className="text-black">{art.createdAt}</TableCell>
+                <TableCell className="text-black text-center">{art.createdAt}</TableCell>
                 <TableCell>
+                  <div className="flex justify-between gap-">
+                    <button className='bg-custom-15616D text-white p-1 rounded-md mr-1' type='button'>
+                      Modifier
+                    </button>
 
-                <button className='bg-custom-15616D text-white p-1 rounded-md' type='button'>
-                  Modifier
-                </button>
-                
-                <button className='ml-2 bg-red-800 text-white p-1 rounded-md' type='button'>
-                  Supprimer
-                </button>
+                    <button className='bg-red-800 text-white p-1 rounded-md ml-1' type='button'>
+                      Supprimer
+                    </button>
+                  </div>
                 </TableCell>
               </TableRow>
             );
           })}
         </TableBody>
+
       </Table>
+
       <div className='flex justify-between mt-4'>
-        <button className='bg-custom-818181 rounded-md text-white p-1' type='button' >Précédent</button>
-        <button className='bg-custom-818181 rounded-md text-white p-1' type='button' >Suivant</button>
+
+        <button
+          className='bg-custom-818181 rounded-md
+        text-white p-1'
+          type='button'
+          onClick={handlePrevious}
+          disabled={startIndex === 0}>
+          <FontAwesomeIcon icon={faArrowLeft} className="mr-2"/>
+          Précédent
+        </button>
+
+        <button
+          className='bg-custom-818181 rounded-md text-white p-1'
+          type='button'
+          onClick={handleNext}
+          disabled={startIndex + newsNbrPerPage >= newsList?.length}>
+          Suivant
+          <FontAwesomeIcon icon={faArrowRight} className="ml-2" />
+        </button>
+
       </div>
     </div>
   );
