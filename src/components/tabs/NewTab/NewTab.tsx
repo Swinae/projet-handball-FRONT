4dcc25c6738b1c57c219971cbf00de26ab75a46a
jordan <jordannigthwatch@hotmail.com>
2024-03-26 09:29:34 +0100

    update: début de dynamisation du composant NewTab

--- src/components/tabs/NewTab/NewTab.tsx
+++ src/components/tabs/NewTab/NewTab.tsx
@@ -1,6 +1,29 @@
 import { Table, TableBody, TableCell, TableHead, TableHeadCell, TableRow } from "flowbite-react";
+import { getNewsList } from "../../../services/api/getNewsList";
+import { useEffect, useState } from "react";
+import { NewData } from "../../../services/interfaces/NewData";
+
+/*à faire: mettre en place la logique pour afficher maximum 6 premieres actua
+puis en cliquant sur suivant il affiche les autres
+*/
 
 export function NewTab() {
+  const[newsList,setNewsList]=useState<NewData[]>();
+
+  useEffect(()=>{
+    const getNewsListRequest=async()=>{
+      const response= await getNewsList();
+      if(response){
+        //update NewsList
+        setNewsList(response);
+      }
+      else{
+        console.log("la requête n'a pas abouti");
+      }
+    }
+    getNewsListRequest();
+  },[])  
+
   return (
     <div className="overflow-x-auto mb-6">
 
@@ -15,54 +38,26 @@ export function NewTab() {
           <TableHeadCell className='bg-custom-15616D' >Actions</TableHeadCell>
         </TableHead>
         <TableBody className="divide-y">
-          <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
-            <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-              {'Apple MacBook Pro 17"'}
-            </TableCell>
-            <TableCell className="text-black">23/12/24</TableCell>
-            <TableCell>
-              <button className='bg-custom-15616D text-white p-1 rounded-md' type='button'>
-                Modifier
-              </button>
+          {newsList?.map((art) => {
+            return (
+              <TableRow key={art.id} className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                  {art.title}
+                </TableCell>
+                <TableCell className="text-black">{art.createdAt}</TableCell>
+                <TableCell>
 
-              <button className='ml-2 bg-red-800 text-white p-1 rounded-md' type='button'>
-                Supprimer
-              </button>
-
-            </TableCell>
-          </TableRow>
-          <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
-            <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-              {'Apple MacBook Pro 17"'}
-            </TableCell>
-            <TableCell className="text-black">23/12/24</TableCell>
-            <TableCell>
-              <button className='bg-custom-15616D text-white p-1 rounded-md' type='button'>
-                Modifier
-              </button>
-
-              <button className='ml-2 bg-red-800 text-white p-1 rounded-md' type='button'>
-                Supprimer
-              </button>
-
-            </TableCell>
-          </TableRow>
-          <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
-            <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-              {'Apple MacBook Pro 17"'}
-            </TableCell>
-            <TableCell className="text-black">23/12/24</TableCell>
-            <TableCell>
-              <button className='bg-custom-15616D text-white p-1 rounded-md' type='button'>
-                Modifier
-              </button>
-
-              <button className='ml-2 bg-red-800 text-white p-1 rounded-md' type='button'>
-                Supprimer
-              </button>
-
-            </TableCell>
-          </TableRow>
+                <button className='bg-custom-15616D text-white p-1 rounded-md' type='button'>
+                  Modifier
+                </button>
+                
+                <button className='ml-2 bg-red-800 text-white p-1 rounded-md' type='button'>
+                  Supprimer
+                </button>
+                </TableCell>
+              </TableRow>
+            );
+          })}
         </TableBody>
       </Table>
       <div className='flex justify-between mt-4'>
