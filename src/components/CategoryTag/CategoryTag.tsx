1d9512d55905e95b732c0a578e19153b6d516a2d
Alexandre BALPE <alexandre.balpe@gmail.com>
2024-03-29 15:05:30 +0100

    ASSO_NW-90 ASSO_NW-88
    Updated :
    - CategoryTag component now change its bg color depending on the category of the event

--- src/components/CategoryTag/CategoryTag.tsx
+++ src/components/CategoryTag/CategoryTag.tsx
@@ -5,12 +5,27 @@ interface EventProps {
 }
 
 export default function CategoryTag(props: EventProps) {
-    
+
     const { eventCategory } = props
+    let categoryTagColor = ''
+    switch (eventCategory.category) {
+        case 'Match':
+            categoryTagColor = 'bg-red-600';
+            break;
+        case "Entraînement":
+            categoryTagColor = "bg-custom-FF7D00";
+            break;
+        case "Apéro":
+            categoryTagColor = "bg-[#78290F]";
+            break;
+        default:
+            categoryTagColor = "bg-gray-500";
+    }
+
 
     return (
         <>
-            <div className="category bg-red-600 top-4 left-4 py-1 px-4 rounded-full text-white w-fit">
+            <div className={`category ${categoryTagColor} top-4 left-4 py-1 px-4 rounded-full text-white w-fit`}>
                 {eventCategory?.category}
             </div>
         </>
