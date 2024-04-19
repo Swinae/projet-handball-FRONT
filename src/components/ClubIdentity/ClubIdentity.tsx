312757189121e4ae40c5bef3ebfdd7408db957d7
Alexandre BALPE <alexandre.balpe@gmail.com>
2024-04-19 17:34:31 +0200

    Added: - Link to the homepage when clicking club logo in the navbar

--- src/components/ClubIdentity/ClubIdentity.tsx
+++ src/components/ClubIdentity/ClubIdentity.tsx
@@ -1,9 +1,10 @@
+import { NavLink } from 'react-router-dom';
 import './ClubIdentity.css';
 
 export function ClubIdentity(){
   return(
     <div className="club-identity">
-      <img src="/logoClub.png" alt="logo du club"/>
+      <NavLink to={'/'}><img src="/logoClub.png" alt="logo du club"/></NavLink>
       <h1 className='text-white text-base md:text-2xl'>NIGHT'S WATCH HANDBALL</h1>
     </div>
   )
