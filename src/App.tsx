384a39c7182d6e16591e802883428bd6dee91243
Alexandre BALPE <alexandre.balpe@gmail.com>
2024-04-21 13:52:13 +0200

    ASSO_NW-62 Added : - Calendar page en calendar component

--- src/App.tsx
+++ src/App.tsx
@@ -15,65 +15,67 @@ import { NewDetailsPage } from './pages/News/NewDetailsPage';
 import DashboardPage from './pages/Dashboard/DashboardPage';
 import EventDetailsPage from './pages/Events/EventDetailsPage';
 import { PlayersPage } from './pages/Players/PlayersPage';
+import { CalendarPage } from './pages/Calendar/CalendarPage';
 
-interface dataUser{
-  id:string,
-  role:string,
-  firstname:string,
-  lastname:string,
-  email:string,
-  password:string,
-  avatar:string,
-  token:string 
+interface dataUser {
+	id: string,
+	role: string,
+	firstname: string,
+	lastname: string,
+	email: string,
+	password: string,
+	avatar: string,
+	token: string
 }
 
 function App() {
-  const [userRole, setUserRole] = useState<string>("visiteur");
-  console.log(userRole);
-  
-  //function to redifine userRole
-  function redifineUserRole(role:string){
-    //redefine role in state userRole
-    setUserRole(role)
-  }
+	const [userRole, setUserRole] = useState<string>("visiteur");
+	console.log(userRole);
 
-  //userData
-  const [userData, setUserData] = useState<dataUser>();
+	//function to redifine userRole
+	function redifineUserRole(role: string) {
+		//redefine role in state userRole
+		setUserRole(role)
+	}
 
-  //function to redefine userData
-  function handleUserData(data:dataUser):void{
-    setUserData(data);
-  }
+	//userData
+	const [userData, setUserData] = useState<dataUser>();
 
-  return (
-    <>
-      <header className='bg-custom-15616D'>
-        <div className='grid grid-cols-1 sm:grid-cols-2 justify-center items-center'>
-          <ClubIdentity />
-          <div className='flex flex-col gap-4 items-end sm:flex-row sm:items-center sm:justify-end'>
-            { (userRole === "admin" || userRole === "joueur" || userRole === "supporter")?<Person avatar={userData?.avatar} firstname={userData?.firstname} lastname={userData?.lastname} role={userData?.role} />:<SignupModal/>}
-            <LoginModal handleUserData={handleUserData} redifineUserRole={redifineUserRole} />
-          </div>
-        </div>
-        <NavBar userRole={userRole} />
-      </header>
-      <main>
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/actualités' element={<NewsPage/>} />
-          <Route path='/actualités/detail/:idNew' element={<NewDetailsPage/>} />
-          <Route path='/évènements' element={<EventsPage />} />
-          <Route path='/évènements/detail/:idEvent' element={<EventDetailsPage/>} />
-          <Route path='/joueurs' element={<PlayersPage />} />
-          <Route path='/dashboard' element={<DashboardPage />} />
-          <Route path='*' element={<NotFoundPage />} />
-        </Routes>
-      </main>
-      <footer >
-        <FooterDown/>
-      </footer>
-    </>
-  )
+	//function to redefine userData
+	function handleUserData(data: dataUser): void {
+		setUserData(data);
+	}
+
+	return (
+		<>
+			<header className='bg-custom-15616D'>
+				<div className='grid grid-cols-1 sm:grid-cols-2 justify-center items-center'>
+					<ClubIdentity />
+					<div className='flex flex-col gap-4 items-end sm:flex-row sm:items-center sm:justify-end'>
+						{(userRole === "admin" || userRole === "joueur" || userRole === "supporter") ? <Person avatar={userData?.avatar} firstname={userData?.firstname} lastname={userData?.lastname} role={userData?.role} /> : <SignupModal />}
+						<LoginModal handleUserData={handleUserData} redifineUserRole={redifineUserRole} />
+					</div>
+				</div>
+				<NavBar userRole={userRole} />
+			</header>
+			<main>
+				<Routes>
+					<Route path='/' element={<HomePage />} />
+					<Route path='/actualités' element={<NewsPage />} />
+					<Route path='/actualités/detail/:idNew' element={<NewDetailsPage />} />
+					<Route path='/évènements' element={<EventsPage />} />
+					<Route path='/évènements/detail/:idEvent' element={<EventDetailsPage />} />
+					<Route path='/joueurs' element={<PlayersPage />} />
+					<Route path='/calendrier' element={<CalendarPage />} />
+					<Route path='/dashboard' element={<DashboardPage />} />
+					<Route path='*' element={<NotFoundPage />} />
+				</Routes>
+			</main>
+			<footer >
+				<FooterDown />
+			</footer>
+		</>
+	)
 }
 
 export default App
---
+++ src/pages/Calendar/CalendarPage.tsx
@@ -0,0 +1,11 @@
+import Calendar from "../../components/Calendar/Calendar";
+
+export function CalendarPage() {
+
+
+    return (
+        <>
+        <Calendar />
+        </>
+    )
+}
