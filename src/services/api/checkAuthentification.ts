6567ad51cffc8e4ab0b9bb9f77ee80bfd6630394
jordan <jordannigthwatch@hotmail.com>
2024-05-10 14:13:28 +0200

    update : insert comment

--- src/services/api/checkAuthentification.ts
+++ src/services/api/checkAuthentification.ts
@@ -1,14 +1,14 @@
-//import { useApi } from "../hooks/useApi";
-import { users } from "../../components/modales/LoginModal/faker";
+import { useApi } from "../hooks/useApi";
+//import { users } from "../../components/modales/LoginModal/faker";
 import { DataConnexion } from "../interfaces/DataConnexion";
 
-//const api = useApi();
+const api = useApi();
 
 export async function checkAuthentification(userLoginData: DataConnexion):Promise<any> {
-  console.log('userLoginData: ',userLoginData);
+  /* console.log('userLoginData: ',userLoginData);
  const userFound=users.find((user) => user.email === userLoginData.email && user.password === userLoginData.password )
- return userFound? userFound:undefined;
-/*   const {data} = await api.post(`auth/login`,userLoginData);
-  console.log("response: ",data);
-  return data; */
+ return userFound? userFound:undefined; */
+  const {data} = await api.post(`auth/login`,userLoginData);
+  //console.log("response: ",data);
+  return data;
 }
