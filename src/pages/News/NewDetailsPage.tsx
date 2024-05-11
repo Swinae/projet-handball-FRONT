4ff5246ed6c938ce3d7ca872fc7965569fbef034
jordan <jordannigthwatch@hotmail.com>
2024-05-11 17:33:02 +0200

    update : inséré une image d'actualité par défaut si aucune n'est fournie

--- src/pages/News/NewDetailsPage.tsx
+++ src/pages/News/NewDetailsPage.tsx
@@ -2,33 +2,35 @@ import { useParams } from "react-router-dom"
 import { getNewDetail } from "../../services/api/News";
 import { useEffect, useState } from "react";
 
-interface newDetail{
-  id:string;
-  img:string;
-  title:string;
-  description:string;
-  created_at:string;
+interface newDetail {
+  id: string;
+  img: string;
+  title: string;
+  content: string;
+  created_at: string;
+  creator_id:number;
+  updated_at:string;
 }
 
-export function NewDetailsPage(){
-  const{idNew}=useParams();
+export function NewDetailsPage() {
+  const { idNew } = useParams();
 
-  const[newDetail,setNewDetail]=useState<newDetail>();
- 
-  useEffect(()=>{
-    if(idNew){
-      const requestApi= async()=>{
-        try{
-          const response=await getNewDetail(idNew);
-          setNewDetail(response);
+  const [newDetail, setNewDetail] = useState<newDetail>();
+
+  useEffect(() => {
+    if (idNew) {
+      const requestApi = async () => {
+        try {
+          const response = await getNewDetail(idNew); console.log(response.data)
+          setNewDetail(response.data);
         }
-        catch(error){
+        catch (error) {
           console.log("La requête a échoué");
         }
       }
       requestApi();
     }
-  },[])
+  }, [])
 
   return (
     <div className="flex justify-center items-center h-full">
@@ -36,9 +38,9 @@ export function NewDetailsPage(){
         <h2 className="text-left text-2xl mb-4 font-bold">
           {newDetail?.title}
         </h2>
-        <img className="rounded" src={"/"+newDetail?.img} alt="Il s'agit d'une illustration de l'actualité"/>
-        <time className="text-right mt-2">{newDetail?.created_at}</time>
-        <p className="mt-2">{newDetail?.description}</p>
+        <img className="rounded" src={newDetail && newDetail.img? newDetail.img: "/img_actu_defaut.jpg"} alt="Il s'agit d'une illustration de l'actualité" />
+        <time className="text-right mt-2">{newDetail?.created_at.split("T")[0].split("-").reverse().join("-")}</time>
+        <p className="mt-2">{newDetail?.content}</p>
       </div>
     </div>
   )
