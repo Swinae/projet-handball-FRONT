1b528a43bd883b02aea355ba392f5393b17c5dd9
Alexandre BALPE <alexandre.balpe@gmail.com>
2024-05-28 14:20:19 +0200

    Updated events API requests

--- src/services/api/Events.ts
+++ src/services/api/Events.ts
@@ -3,28 +3,30 @@ import { useApi } from "../hooks/useApi";
 
 const api = useApi()
 
-export async function getEvents(): Promise<any> {
+export async function getEvents(): Promise<IEventInterface> {
     const { data } = await api.get(`event`);
     return data;
 }
 
-export async function getEventById(eventId: number): Promise<IEventInterface | undefined> {
+export async function getEventById(eventId: number): Promise<IEventInterface> {
+    console.log('je suis la');
     const { data } = await api.get(`${eventId}/:id`)
+    console.log(data);
     return data
 }
 
-export async function postEvents(event: IEventInterface): Promise<any> {
-    const postEvent = await api.post('event', event)
-    return postEvent
+export async function postEvents(event: IEventInterface): Promise<IEventInterface> {
+    const { data } = await api.post('event', event)
+    return data
 }
 
-export async function putEvents(modifiedEvent: IEventInterface): Promise<any> {
+export async function putEvents(modifiedEvent: IEventInterface): Promise<IEventInterface> {
     const eventId = modifiedEvent.id
-    const updatedEvent = await api.put(`event/:${eventId}`)
-    return updatedEvent
+    const { data }  = await api.put(`event/:${eventId}`)
+    return data
 }
 
-export async function deleteEvent(eventId: number): Promise<any> {
-    const deletedEvent = await api.delete(`event/:${eventId}`)
-    return deletedEvent
+export async function deleteEvent(eventId: number): Promise<IEventInterface> {
+    const { data }  = await api.delete(`event/:${eventId}`)
+    return data
 }
